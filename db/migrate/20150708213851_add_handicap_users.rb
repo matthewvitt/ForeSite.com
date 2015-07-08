261913b44176a147126b352df1a5a0f64b686a73
matthewvitt <matthewvitt@gmail.com>
2015-07-08 14:44:04 -0700

    add handicap

---
+++ db/migrate/20150708213851_add_handicap_users.rb
@@ -0,0 +1,4 @@
+class AddHandicapUsers < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150607205326) do
+ActiveRecord::Schema.define(version: 20150708214318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20150607205326) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "handicap"
   end
 
 end
