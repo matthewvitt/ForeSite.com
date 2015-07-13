edb4c015c9d7cc736d302fc4fc1cfb4ac3a32365
matthewvitt <matthewvitt@gmail.com>
2015-07-13 16:43:11 -0700

    navbar(kindof)

---
+++ config/initializers/date_format.rb
@@ -0,0 +1,8 @@
+# Date
+Date::DATE_FORMATS[:default] = "%m/%d/%Y"
+
+# Time
+Time::DATE_FORMATS[:default] = "%m/%d/%Y %H:%M"
+
+#DateTime
+DateTime::DATE_FORMATS[:default] = "%B %e, %Y" # November 3, 2013 14:22
